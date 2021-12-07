6d5378f5122d47d3708f7e919ac96ac0b0d2482d
Azrilhidayatullah24 <azrilhidayatullah24@gmail.com>
2021-12-07 16:54:13 +0700

    Menambah Fungsi Add Foto Profile

--- MomokasSC/app/src/main/java/com/capstone/momokas/ui/profile/ProfileFragment.kt
+++ MomokasSC/app/src/main/java/com/capstone/momokas/ui/profile/ProfileFragment.kt
@@ -1,6 +1,9 @@
 package com.capstone.momokas.ui.profile
 
+import android.Manifest
+import android.app.Activity
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Bundle
 import android.util.Log
@@ -12,6 +15,8 @@ import android.widget.ProgressBar
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.capstone.momokas.R
@@ -26,7 +31,11 @@ import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.database.*
 import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.StorageReference
+import com.google.firebase.storage.UploadTask
+import com.squareup.picasso.Picasso
+import com.theartofdev.edmodo.cropper.CropImage
 import de.hdodenhof.circleimageview.CircleImageView
+import java.util.*
 
 class ProfileFragment : Fragment() {
 
@@ -45,11 +54,10 @@ class ProfileFragment : Fragment() {
     private var getRefenence: DatabaseReference? = null
     private var references: StorageReference? = null
 
-    private val GetUserID: String? = null
     private var profileImage: CircleImageView? = null
     private var foto: FloatingActionButton? = null
     private var progressBar: ProgressBar? = null
-    private val mImageUri: Uri? = null
+    private var mImageUri: Uri? = null
     var PERMISSION_STORAGE = 1
     var GALLERY_PICK = 2
 
@@ -83,11 +91,11 @@ class ProfileFragment : Fragment() {
         //Mendapatkan Referensi dari Firebase Storage
         references = FirebaseStorage.getInstance().reference
         database = FirebaseDatabase.getInstance().reference
-
-
+        foto?.setOnClickListener { view: View? ->
+            getPhotoFromStorage(view)
+        }
+        getphoto()
         GetData()
-
-
 
         val logout = root?.findViewById<MaterialButton>(R.id.btn_logout)
         logout?.setOnClickListener {
@@ -113,6 +121,7 @@ class ProfileFragment : Fragment() {
 
         return root
     }
+
 
     private fun GetData() {
         //Mendapatkan Referensi Database
@@ -140,5 +149,104 @@ class ProfileFragment : Fragment() {
             })
     }
 
+    //Method Ini Digunakan Untuk Membuka Image dari Galeri atau Kamera
+    //Method Ini Digunakan Untuk Menapatkan Hasil pada Activity, dari Proses Yang kita buat sebelumnya
+    //Dan Mendapatkan Hasil File Photo dari Galeri atau Kamera
+    private fun getPhotoFromStorage(view: View?) {
+        if (view?.let {
+                ContextCompat.checkSelfPermission(
+                    it.context, Manifest.permission.READ_EXTERNAL_STORAGE
+                )
+            } != PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(
+                requireContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                (view?.context as Activity), arrayOf(
+                    Manifest.permission.READ_EXTERNAL_STORAGE,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+                ),
+                PERMISSION_STORAGE
+            )
+        } else {
+            val galleryIntent = Intent()
+            galleryIntent.type = "image/*"
+            galleryIntent.action = Intent.ACTION_GET_CONTENT
+            startActivityForResult(
+                Intent.createChooser(galleryIntent, "SELECT IMAGE"),
+               GALLERY_PICK
+            )
+        }
+    }
+
+
+    private fun uploadImage() {
+        progressBar!!.visibility = View.VISIBLE
+        val imageURL = UUID.randomUUID().toString()
+        val imagePath = photoReference.child(imageURL)
+        imagePath.putFile(mImageUri!!)
+            .addOnCompleteListener { task: Task<UploadTask.TaskSnapshot?> ->
+                if (task.isSuccessful) {
+                    photoReference.child(imageURL).downloadUrl
+                        .addOnSuccessListener { uri: Uri ->
+                            val downloadUrl = uri.toString()
+                            database!!.child("PhotoProfil").child(auth!!.uid)
+                                .setValue(ImageModel(downloadUrl))
+                                .addOnSuccessListener { aVoid: Void? ->
+                                    //Peristiwa ini terjadi saat user berhasil menyimpan datanya kedalam Database
+                                    Toast.makeText(context, "Upload Berhasil", Toast.LENGTH_SHORT)
+                                        .show()
+                                    getphoto()
+                                }
+                        }
+                } else {
+                    progressBar!!.visibility = GONE
+                    Toast.makeText(
+                        context, "Terjadi Kesalahan Silakan Coba Lagi",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == GALLERY_PICK && resultCode == Activity.RESULT_OK) {
+            val imageUri = Objects.requireNonNull(data)?.data
+            requireContext().let {
+                CropImage.activity(imageUri)
+                    .setAspectRatio(1, 1)
+                    .setMinCropWindowSize(200, 200)
+                    .start(it, this)
+            }
+        }
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+            val result = CropImage.getActivityResult(data)
+            if (resultCode == Activity.RESULT_OK) {
+                mImageUri = Objects.requireNonNull(result).uri
+                uploadImage()
+            }
+        }
+    }
+
+    private fun getphoto() {
+        reference = FirebaseDatabase.getInstance().reference
+        reference!!.child("PhotoProfil").child(auth!!.uid)
+            .addValueEventListener(object : ValueEventListener {
+                override fun onDataChange(dataSnapshot: DataSnapshot) {
+                    val dataprofile: ImageModel? = dataSnapshot.getValue(ImageModel::class.java)
+                    if ((if (dataprofile != null) dataprofile.image_url else "NULL") != null) {
+                        Picasso.get()
+                            .load(if (dataprofile != null) dataprofile.image_url else "NULL")
+                            .placeholder(R.drawable.ic_baseline_account_circle_200)
+                            .into(profileImage)
+                        progressBar!!.visibility = GONE
+                    }
+                }
+
+                override fun onCancelled(error: DatabaseError) {}
+            }
+            )
+    }
 
 }
